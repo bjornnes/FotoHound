47dc62f7cefcc824c68f4e177c0d17c77e262688
Stein-Erik Bjørnnes <steinerikbjornnes@gmail.com>
2017-04-04 10:05:01 +0200

    Updating development branch with new changes

--- handlers/DataHandler.js
+++ handlers/DataHandler.js
@@ -11,6 +11,8 @@ var pool = mysql.createPool({
 
 var DataHandler = function() {
    this.data = getAllData;
+   this.size = getSize;
+   this.insert = insertData;
 };
 
 function getAllData(req, res){
@@ -40,4 +42,73 @@ function getAllData(req, res){
     }
   )
 };
+
+//Get with paramters in URL
+function getSize(req,res){
+  var name=req.params.name;
+
+  var sql = "select data.size from data where data.name = ?";
+  var inserts = [name];
+  sql=mysql.format(sql, inserts);
+
+  pool.getConnection().then(
+    function(connection){
+      console.log('connected getSize');
+      connection.query(sql).then(
+        function(rows){
+          res.json(200, rows);
+          connection.destroy();
+          return;
+        },
+        function(error){
+          console.log('error quering');
+          res.json(500, error);
+          connection.destroy();
+          return;
+        }
+      )
+    },
+    function(error){
+      console.log('error getting connection');
+      res.json(500, error);
+      return;
+    }
+  )
+};
+
+function insertData(req,res){
+  var name=req.body.name;
+  var text = req.body.text;
+  var size = req.body.size;
+
+  var sql = "insert into data (name, text, size) values (?,?,?)";
+  var inserts=[name, text, size];
+  sql=mysql.format(sql, inserts);
+
+  pool.getConnection().then(
+    function(connection){
+      console.log('connected');
+      connection.query(sql).then(
+        function(rows){
+          res.json(200, rows);
+          connection.destroy();
+          return;
+        },
+        function(error){
+          console.log('error quering');
+          res.json(500, error);
+          connection.destroy();
+          return;
+        }
+      )
+    },
+    function(error){
+      console.log('error getting connection');
+      res.json(500, error);
+      return;
+    }
+  )
+};
+
+
 module.exports = DataHandler;
