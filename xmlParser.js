666d966a5e9c9a20123218771eadc92df30e728e
Andreas Bergman <andreasbergman93@gmail.com>
2017-05-01 15:13:34 +0200

    begin sorting after descritpion

--- xmlParser.js
+++ xmlParser.js
@@ -1,27 +1,33 @@
 var DOMParser = require('xmldom').DOMParser;
-//var xmlFile = require('./DocInfoFromIM_1.txt',)
 var parser1 = require('xml2json');
-
-
 var fs = require('fs'),
     xml2js = require('xml2js');
-var xml = "<root>Hello xml2js!</root>";
-var parser = new xml2js.Parser();
-fs.readFile('2017-04-28T133500Z_643870646_RC1E932D4950_RTRMADP_0_FRANCE-ELECTION-ZIDANE.XML', function(err, data) {
-    parser.parseString(data, function (err, result) {
-      //var json = result.DocInfo.XMP;
-        console.log(JSON.stringify(result.newsMessage.itemSet[0].newsItem[0].contentMeta[0].description[0]._));
-        console.log('Done');
-    });
+
+var relate = require('./handlers/queryHandler');
+var lang = 'eng';
+var maps = {};
+var words;
+var description;
+
+relate.findRelatedWords('english', lang, function(res){
+  words = res;
+  var parser = new xml2js.Parser();
+  fs.readFile('DocInfoFromIM_3.xml', function(err, data) {
+      parser.parseString(data, function (err, result) {
+          console.log(result.DocInfo.XMP);
+
+          //var description = result.newsMessage.itemSet[0].newsItem[0].contentMeta[0].description[0]._.toLowerCase();
+          // maps['2017-04-28T133500Z_643870646_RC1E932D4950_RTRMADP_0_FRANCE-ELECTION-ZIDANE.XML'] = Number(1.5);
+          // for( i in words){
+          //   if(description.indexOf(words[i].word) > -1){
+          //       var value = maps['2017-04-28T133500Z_643870646_RC1E932D4950_RTRMADP_0_FRANCE-ELECTION-ZIDANE.XML'];
+          //       maps['2017-04-28T133500Z_643870646_RC1E932D4950_RTRMADP_0_FRANCE-ELECTION-ZIDANE.XML'] = Number(value + 1);
+          //       //steg 1 lage score for hvert bilde
+          //       // steg 2 map, filnavn som key og prob er value
+          //     console.log(maps['2017-04-28T133500Z_643870646_RC1E932D4950_RTRMADP_0_FRANCE-ELECTION-ZIDANE.XML']);
+          //   }
+          // }
+      });
+  });
+  //console.log(words);
 });
-//console.log("input -> %s", xml)
-
-// xml to json
-//var json = parser.toJson(test);
-//console.log("to json -> %s", json);
-
-// var doc = new DOMParser().parseFromString(test,'text/xml');
-//     doc.documentElement.setAttribute('x','y');
-//     doc.documentElement.setAttributeNS('./lite','c:x','y2');
-//     var nsAttr = doc.documentElement.getAttributeNS('./lite','x')
-    //console.info(nsAttr)
