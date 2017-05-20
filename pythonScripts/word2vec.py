e51448927c800e9b7488cfbf08752256136aeb6e
bjornnes <steinerikbjornnes@gmail.com>
2017-05-20 17:19:32 +0200

    Update word2vec.py
    added comments

--- pythonScripts/word2vec.py
+++ pythonScripts/word2vec.py
@@ -7,14 +7,11 @@ if __name__ == '__main__':
     from gensim.models import KeyedVectors
     import logging
 
-    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(messages)s', level=logging.INFO)
-    sentences = word2vec.Text8Corpus('~/Documents/text8')
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(messages)s', level=logging.INFO)#set logging
+    sentences = word2vec.Text8Corpus('~/Documents/text8')#Import and reference the corpus
 
-    model = word2vec.Word2Vec(sentences, size=100)
-    model.wv.save_word2vec_format('../../text8.bin', binary=True)
-
-    model = KeyedVectors.load_word2vec_format('../../text8.bin', binary=True)
-
-    print (sys.argv)
-    print (model.most_similar('man',numbers = 10))
+    model = word2vec.Word2Vec(sentences, size=100)#set size of vector to 300 and train model
+    model.wv.save_word2vec_format('../../text8.bin', binary=True)#save model to disk
+    
+    print (model.most_similar('man'))#print most similar words to the word man
     sys.exit(main(sys.argv))
