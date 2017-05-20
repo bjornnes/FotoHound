c94b9d55e005230f019685a28f3fc2af29861c72
bjornnes <steinerikbjornnes@gmail.com>
2017-05-20 17:20:43 +0200

    Update fasttextt.py
    added comments

--- pythonScripts/fasttextt.py
+++ pythonScripts/fasttextt.py
@@ -6,9 +6,9 @@ if __name__ == '__main__':
 	import fasttext
 	import logging
 
-	logging.basicConfig(format='%(asctime)s : %(levelname)s : %(messages)s', level=logging.INFO)
+	logging.basicConfig(format='%(asctime)s : %(levelname)s : %(messages)s', level=logging.INFO) #set logging
 
-	model = fasttext.skipgram('../../text8', 'text8-fasttext-skipgram')
-	print(model.words)
+	model = fasttext.skipgram('../../text8', 'text8-fasttext', dim=300)#Training with skipgram using vectors with dimension of 300. 2nd param is name of output file
+	print(model.most_similar('man')) #prints most similar words to the word man
 
 	sys.exit(main(sys.args))
